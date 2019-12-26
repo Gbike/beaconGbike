0499c78df6bbff8e0414f618bced76ab1e1c60e4
suban1597 <59216475+suban1597@users.noreply.github.com>
2019-12-26 14:56:02 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -16,7 +16,7 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 var_export($request_array);
 
-$new_date = get_the_date('d-m-Y', strtotime($_POST['dateFrom']));
+$new_date = date("h:i:sa");
 echo $new_date;
 
 $jsonFlex = [
