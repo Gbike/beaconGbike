04356732546ee61336fb9e970e7ad252df9afb29
suban1597 <59216475+suban1597@users.noreply.github.com>
2019-12-26 18:56:59 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -141,8 +141,8 @@ echo "OK";
 function send_reply_message($url, $post_header, $post_body)
 {
     $ch = curl_init($url);
-//    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
