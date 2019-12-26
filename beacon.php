ddc27af0c9e3145073de5dcbc537c357dd8bedb1
suban1597 <59216475+suban1597@users.noreply.github.com>
2019-12-26 14:01:11 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -32,7 +32,7 @@ $jsonFlex = [
           ],
           [
             "type" => "text",
-            "text" => "ทดสอบ 00:00:00",
+            "date" => "ms-since-epoch",
             "size" => "3xl",
             "weight" => "bold",
             "color" => "#000000"
